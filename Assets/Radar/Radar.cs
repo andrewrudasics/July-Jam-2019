1a3299de665cf1434a68dfb97acd5b77a3291f56
Jose Pacio <pacioj@uw.edu>
2019-07-27 21:43:58 -0700

    radar is working

---
+++ Assets/Radar/Radar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Radar : MonoBehaviour
+{
+    public bool Debug;
+    [Space()]
+    public GameObject origin;
+    public GameObject blip;
+    public float range = 10;
+
+    Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying) {
+            mat = GetComponent<Renderer>().sharedMaterial;
+        }
+
+        if (mat != null && origin != null && blip != null)
+        {
+            Vector3 location = blip.transform.position - origin.transform.position;
+            location.y = 0;
+            location = Quaternion.Euler(0,-transform.eulerAngles.y,0) * location;
+            Vector2 projection = new Vector2(location.x, location.z);
+            projection = projection.magnitude / (range * 2) * projection.normalized;
+            mat.SetVector("_Location", projection);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        DebugExtension.DrawCircle(origin.transform.position, Vector3.up, Color.red, range);
+    }
+}
