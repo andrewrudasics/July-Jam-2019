254b84101bcd7b60d713b55dc0923d7855b35813
Jose Pacio <pacioj@uw.edu>
2019-08-05 13:04:24 -0700

    new model and nav mesh work on the map

---
+++ Assets/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Collectable : MonoBehaviour
+{
+    public UnityEvent collectedUnityEvent;
+    public delegate void CollectableEvent();
+    public CollectableEvent collected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (collected != null) {
+            collected.Invoke();
+        }
+        collectedUnityEvent.Invoke();
+
+        Destroy(this.gameObject);
+    }
+}
